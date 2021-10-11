a5aaf873bc7935a6cb928ae9403e9feb454b2036
Scott Wakefield <scott@expertcc.co.uk>
2021-10-11 15:10:25 +0100

    FAI-80 empty test for handler.

---
+++ src/SFA.DAS.FAA.Application.UnitTests/Vacancies/Queries/WhenSearchingApprenticeshipVacancies.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Application.UnitTests.Vacancies.Queries
+{
+    public class WhenSearchingApprenticeshipVacancies
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Vacancies_From_Repository()
+        {
+            
+        }
+    }
+}
