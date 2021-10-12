574e90dd6e932b04ffcec1ffda36bdf1d8889535
Scott Wakefield <scott@expertcc.co.uk>
2021-10-12 15:51:52 +0100

    FAI-80 casting operaters for api responses.

---
+++ src/SFA.DAS.FAA.Api.UnitTests/ApiResponses/WhenCastingToGetApprenticeshipVacancyResponse.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAA.Api.ApiResponses;
+using SFA.DAS.FAA.Domain.Entities;
+
+namespace SFA.DAS.FAA.Api.UnitTests.ApiResponses
+{
+    public class WhenCastingToGetApprenticeshipVacancyResponse
+    {
+        [Test, AutoData]
+        public void Then_Maps_Fields(
+            ApprenticeshipSearchItem source)
+        {
+            var response = (GetApprenticeshipVacancyResponse)source;
+
+            response.Should().BeEquivalentTo(source);
+        }
+    }
+}
