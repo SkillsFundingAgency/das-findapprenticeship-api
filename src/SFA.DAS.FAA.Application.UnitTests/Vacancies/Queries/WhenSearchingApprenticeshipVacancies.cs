29736c84c0511fc19fdbd4f4d3ed8f089954c3a3
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-12-08 15:06:29 +0000

    FAI-1022 add controller and query unit tests

--- src/SFA.DAS.FAA.Application.UnitTests/Vacancies/Queries/WhenSearchingApprenticeshipVacancies.cs
+++ src/SFA.DAS.FAA.Application.UnitTests/Vacancies/Queries/WhenSearchingApprenticeshipVacancies.cs
@@ -15,7 +15,7 @@ namespace SFA.DAS.FAA.Application.UnitTests.Vacancies.Queries
     public class WhenSearchingApprenticeshipVacancies
     {
         [Test, MoqAutoData]
-        public async Task Then_Gets_Vacancies_From_Repository(
+        public async Task And_The_Source_Is_Elastic_Then_Gets_Vacancies_From_Repository(
             SearchApprenticeshipVacanciesQuery query,
             ApprenticeshipSearchResponse responseFromRepository,
             [Frozen] Mock<IVacancySearchRepository> mockVacancyIndexRepository,
@@ -23,7 +23,7 @@ namespace SFA.DAS.FAA.Application.UnitTests.Vacancies.Queries
         {
             query.Source = "Elastic";
             mockVacancyIndexRepository
-                .Setup(repository => repository.Find(It.Is<FindVacanciesModel>(c=> 
+                .Setup(repository => repository.Find(It.Is<FindVacanciesModel>(c =>
                         c.PageNumber.Equals(query.PageNumber) &&
                         c.PageSize.Equals(query.PageSize) &&
                         c.Ukprn.Equals(query.Ukprn) &&
@@ -39,7 +39,41 @@ namespace SFA.DAS.FAA.Application.UnitTests.Vacancies.Queries
                         c.VacancySort.Equals(query.VacancySort)
                         )))
                 .ReturnsAsync(responseFromRepository);
-            
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.ApprenticeshipVacancies
+                .Should().BeEquivalentTo(responseFromRepository.ApprenticeshipVacancies);
+            result.TotalFound.Should().Be(responseFromRepository.TotalFound);
+            result.Total.Should().Be(responseFromRepository.Total);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_The_Source_Is_Acs_Then_Gets_Vacancies_From_Repository(
+            SearchApprenticeshipVacanciesQuery query,
+            ApprenticeshipSearchResponse responseFromRepository,
+            [Frozen] Mock<IAcsVacancySearchRespository> mockAcsVacancySearchRepository,
+            SearchApprenticeshipVacanciesQueryHandler handler)
+        {
+            query.Source = "Acs";
+            mockAcsVacancySearchRepository
+                .Setup(repository => repository.Find(It.Is<FindVacanciesModel>(c =>
+                        c.PageNumber.Equals(query.PageNumber) &&
+                        c.PageSize.Equals(query.PageSize) &&
+                        c.Ukprn.Equals(query.Ukprn) &&
+                        c.AccountPublicHashedId.Equals(query.AccountPublicHashedId) &&
+                        c.AccountLegalEntityPublicHashedId.Equals(query.AccountLegalEntityPublicHashedId) &&
+                        c.StandardLarsCode.Equals(query.StandardLarsCode) &&
+                        c.Categories.Equals(query.Categories) &&
+                        c.Lat.Equals(query.Lat) &&
+                        c.Lon.Equals(query.Lon) &&
+                        c.DistanceInMiles.Equals(query.DistanceInMiles) &&
+                        c.NationWideOnly.Equals(query.NationWideOnly) &&
+                        c.PostedInLastNumberOfDays.Equals(query.PostedInLastNumberOfDays) &&
+                        c.VacancySort.Equals(query.VacancySort)
+                        )))
+                .ReturnsAsync(responseFromRepository);
+
             var result = await handler.Handle(query, CancellationToken.None);
 
             result.ApprenticeshipVacancies
