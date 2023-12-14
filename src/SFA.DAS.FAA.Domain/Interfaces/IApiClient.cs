51f0afcc1d464f9721d85f2d304f800b18736f4b
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2023-12-14 22:16:04 +0000

    FAI-1022 - Remove un-used files

--- src/SFA.DAS.FAA.Domain/Interfaces/IApiClient.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SFA.DAS.FAA.Domain.Interfaces;
-public interface IApiClient
-{
-    Task<ApiResponse<TResponse>> Get<TResponse>(IGetApiRequest request);
-}
