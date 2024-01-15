b5d424572d5a0f34e2037a04bf7ac3017ddd6206
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-01-15 15:02:25 +0000

    FAI-1102 add nums to allowed chars in search string

--- src/SFA.DAS.FAA.Data/AzureSearch/AzureSearchHelper.cs
+++ src/SFA.DAS.FAA.Data/AzureSearch/AzureSearchHelper.cs
@@ -59,12 +59,12 @@ public class AzureSearchHelper : IAzureSearchHelper
 
         if (findVacanciesModel.Lat.HasValue && findVacanciesModel.Lon.HasValue)
         {
-            result.ForEach(c=>c.SearchGeoPoint = new GeoPoint{Lat= findVacanciesModel.Lat.Value, Lon = findVacanciesModel.Lon.Value});
+            result.ForEach(c => c.SearchGeoPoint = new GeoPoint { Lat = findVacanciesModel.Lat.Value, Lon = findVacanciesModel.Lon.Value });
         }
 
         return new ApprenticeshipSearchResponse
         {
-            ApprenticeshipVacancies = result.Select(c=>c)
+            ApprenticeshipVacancies = result.Select(c => c)
                 .ToList(),
             TotalFound = Convert.ToInt32(searchResults.Value.TotalCount),
             Total = Convert.ToInt32(totalVacanciesCount)
@@ -88,14 +88,16 @@ public class AzureSearchHelper : IAzureSearchHelper
     {
         if (string.IsNullOrEmpty(searchTerm)) { return "*"; }
 
-        var alphaRegex = new Regex("[a-zA-Z ]");
+        var alphaRegex = new Regex("[a-zA-Z0-9 ]");
         var illegalChars = searchTerm.Where(x => !alphaRegex.IsMatch(x.ToString())).ToList();
-        foreach (var character in searchTerm)
+
+        while (illegalChars.Contains(searchTerm[0]))
         {
-            if (illegalChars.Contains(character))
-            {
-                searchTerm = searchTerm.Replace($"{character}", string.Empty);
-            }
+            searchTerm = searchTerm.Substring(1);
+        }
+        while (illegalChars.Contains(searchTerm[searchTerm.Length - 1]))
+        {
+            searchTerm = searchTerm.Substring(0, searchTerm.Length - 1);
         }
         return string.IsNullOrEmpty(searchTerm) ? "*" : $"\"{searchTerm}*\"";
     }
