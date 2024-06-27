21a63460067926a4406a0b062cce9e0485dbbde5
Chris Foster <chrisfoster186@googlemail.com>
2024-06-27 14:28:46 +0100

    Changes to response model

--- src/SFA.DAS.FAA.Api/ApiResponses/GetApprenticeshipVacanciesByReferenceApiResponse.cs
+++ src/SFA.DAS.FAA.Api/ApiResponses/GetApprenticeshipVacanciesByReferenceApiResponse.cs
@@ -15,9 +15,17 @@ namespace SFA.DAS.FAA.Api.ApiResponses
             public string EmployerName { get; set; }
             public string Title { get; set; }
             public DateTime ClosingDate { get; set; }
-            public string City { get; set; }
-            public string Postcode { get; set; }
             public string ApplicationUrl { get; set; }
+            public Address Address { get; set; }
+        }
+
+        public class Address
+        {
+            public string AddressLine1 { get; set; }
+            public string AddressLine2 { get; set; }
+            public string AddressLine3 { get; set; }
+            public string AddressLine4 { get; set; }
+            public string Postcode { get; set; }
         }
 
         public static implicit operator GetApprenticeshipVacanciesByReferenceApiResponse(
@@ -31,9 +39,15 @@ namespace SFA.DAS.FAA.Api.ApiResponses
                     VacancyReference = x.VacancyReference,
                     Title = x.Title,
                     ClosingDate = x.ClosingDate,
-                    City = x.City,
-                    Postcode = x.Postcode,
-                    ApplicationUrl = x.ApplicationUrl
+                    ApplicationUrl = x.ApplicationUrl,
+                    Address = new Address
+                    {
+                        AddressLine1 = x.Address.AddressLine1,
+                        AddressLine2 = x.Address.AddressLine2,
+                        AddressLine3 = x.Address.AddressLine3,
+                        AddressLine4 = x.Address.AddressLine4,
+                        Postcode = x.Address.Postcode
+                    }
                 })
             };
         }
