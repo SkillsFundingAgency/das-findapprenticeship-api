412d8fc95158e74a8bf57a30865e9de52bf405ba
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-07-08 16:36:10 +0100

    Minor correction

--- src/SFA.DAS.FAA.Application/Vacancies/Queries/GetApprenticeshipVacancyCount/GetApprenticeshipVacancyCountQueryHandler.cs
+++ src/SFA.DAS.FAA.Application/Vacancies/Queries/GetApprenticeshipVacancyCount/GetApprenticeshipVacancyCountQueryHandler.cs
@@ -18,8 +18,6 @@ namespace SFA.DAS.FAA.Application.Vacancies.Queries.GetApprenticeshipVacancyCoun
         }
         public async Task<int> Handle(GetApprenticeshipVacancyCountQuery request, CancellationToken cancellationToken)
         {
-            var result = await _acsVacancySearchRepository.GetHealthCheckStatus(cancellationToken);
-
             if (request.Source == SearchSource.Elastic)
             {
                 return await _vacancySearchRepository.Count();
