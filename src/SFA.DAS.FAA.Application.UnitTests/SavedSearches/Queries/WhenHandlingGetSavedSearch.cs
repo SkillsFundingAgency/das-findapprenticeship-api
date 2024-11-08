151efac4e6e69f65b55cd915f34f3ee13f1819d2
killij <51908793+killij@users.noreply.github.com>
2024-11-08 11:06:15 +0000

    FAI-1949 - Improve test coverage

---
+++ src/SFA.DAS.FAA.Application.UnitTests/SavedSearches/Queries/WhenHandlingGetSavedSearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.FAA.Application.SavedSearches.Queries.GetSavedSearch;
+using SFA.DAS.FAA.Application.SavedSearches.Queries.GetSavedSearchesByUserReference;
+using SFA.DAS.FAA.Data.SavedSearch;
+using SFA.DAS.FAA.Domain.Entities;
+using SFA.DAS.FAA.Domain.Models;
+
+namespace SFA.DAS.FAA.Application.UnitTests.SavedSearches.Queries;
+
+public class WhenHandlingGetSavedSearch
+{
+    [Test, MoqAutoData]
+    public async Task Then_A_Result_Is_Returned(
+        SavedSearchEntity savedSearch,
+        GetSavedSearchQuery query,
+        SearchParameters searchParameters,
+        [Frozen] Mock<ISavedSearchRepository> savedSearchRepository,
+        GetSavedSearchQueryHandler sut
+    )
+    {
+        // arrange
+        savedSearch.SearchParameters = searchParameters.ToJson();
+        savedSearchRepository
+            .Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<Guid>(), default))
+            .ReturnsAsync(savedSearch);
+        
+        // act
+        var results = await sut.Handle(query, default);
+
+        // assert
+        results.SavedSearch.Should().BeEquivalentTo(savedSearch, options => 
+            options.Excluding(x => x.SearchParameters).WithMapping("UserRef", "UserReference"));
+        results.SavedSearch!.SearchParameters.Should().BeEquivalentTo(searchParameters);
+        savedSearchRepository.Verify(x => 
+            x.Get(It.Is<Guid>(id => id == query.UserReference), It.Is<Guid>(id => id == query.Id), default),
+            Times.Once);
+    }
+}
