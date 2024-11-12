cc29faff9a58f3e6e014c0ca4e6c13305c3d90b9
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-11-12 16:16:39 +0000

    Unit test cases added

---
+++ src/SFA.DAS.FAA.Data.UnitTests/ExtensionTests/WhenGettingDescriptionForEnum.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAA.Data.Extensions;
+using SFA.DAS.FAA.Domain.Models;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAA.Data.UnitTests.ExtensionTests
+{
+    [TestFixture]
+    public class WhenGettingDescriptionForEnum
+    {
+        [Test]
+        [MoqInlineAutoData(WageType.CompetitiveSalary, "Competitive Salary")]
+        [MoqInlineAutoData(WageType.Unknown, "Unknown")]
+        [MoqInlineAutoData(WageType.NationalMinimumWage, "National Minimum Wage")]
+        [MoqInlineAutoData(WageType.NationalMinimumWageForApprentices, "National Minimum Wage For Apprentices")]
+        [MoqInlineAutoData(WageType.FixedWage, "Fixed Wage")]
+        public void Then_The_Description_Is_Returned_Correctly(
+            WageType wageType,
+            string expectedDescription)
+        {
+            // sut
+            var sut = wageType.GetDescription();
+
+            // assert
+            sut.Should().Be(expectedDescription);
+        }
+    }
+}
