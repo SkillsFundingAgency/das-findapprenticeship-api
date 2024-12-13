a760491aa3a3c414a480dd78e1f9adc5936b80ba
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-12-13 12:59:53 +0000

    Added condition to get the NHS vacancy by reference
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-2162

---
+++ src/SFA.DAS.FAA.Application.UnitTests/Vacancies/Queries/WhenMappingApprenticeSearchItemToApprenticeshipVacancyItem.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.FAA.Domain.Entities;
+
+namespace SFA.DAS.FAA.Application.UnitTests.Vacancies.Queries
+{
+    [TestFixture]
+    public class WhenMappingApprenticeSearchItemToApprenticeshipVacancyItem
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(ApprenticeshipSearchItem source)
+        {
+            // Act
+            var result = ApprenticeshipVacancyItem.FromApprenticeshipSearchItem(source);
+
+            result.Should().BeEquivalentTo(source);
+        }
+    }
+}
--- src/SFA.DAS.FAA.Domain/Interfaces/IAcsVacancySearchRepository.cs
+++ src/SFA.DAS.FAA.Domain/Interfaces/IAcsVacancySearchRepository.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.FAA.Domain.Entities;
+﻿using System.Collections.Generic;
+using SFA.DAS.FAA.Domain.Entities;
 using SFA.DAS.FAA.Domain.Models;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ public interface IAcsVacancySearchRepository
 {
     Task<ApprenticeshipSearchResponse> Find(FindVacanciesModel findVacanciesModel);
     Task<ApprenticeshipVacancyItem> Get(string vacancyReference);
+    Task<List<ApprenticeshipSearchItem>> GetAll(List<string> vacancyReferences);
     Task<int> Count(FindVacanciesCountModel findVacanciesCountModel);
     Task<HealthCheckResult> GetHealthCheckStatus(CancellationToken cancellationToken);
 }
