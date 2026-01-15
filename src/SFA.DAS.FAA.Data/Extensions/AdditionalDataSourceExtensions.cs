8d4f3b1a601eb58d821ab866b9a43124b410a69c
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-01-15 15:02:00 +0000

    Added comments for future references

--- src/SFA.DAS.FAA.Data/Extensions/AdditionalDataSourceExtensions.cs
+++ src/SFA.DAS.FAA.Data/Extensions/AdditionalDataSourceExtensions.cs
@@ -5,12 +5,13 @@ namespace SFA.DAS.FAA.Data.Extensions
 {
     public static class AdditionalDataSourceExtensions
     {
+        // Maps DataSource enum values to their corresponding Azure Search terms.
         public static string GetAzureSearchTerm(this DataSource source)
         {
             return source switch
             {
-                DataSource.Raa => "RAA",
-                DataSource.Nhs => "Nhs",
+                DataSource.Raa => "RAA", // Note: 'RAA' is the correct casing for Azure Search. FAI-2998
+                DataSource.Nhs => "Nhs", // Note: 'Nhs' is the correct casing for Azure Search. FAI-2998
                 _ => throw new InvalidOperationException("Unable to map data source to azure search term")
             };
         }
